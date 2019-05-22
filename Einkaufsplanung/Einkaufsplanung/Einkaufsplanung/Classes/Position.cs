4c1d4e229b2aef357c0ccedcec6e83de11045b6b
Lukas Meissner <49225138+meissnl@users.noreply.github.com>
2019-05-22 09:30:32 +0200

    Added first implementation to Position

--- Einkaufsplanung/Einkaufsplanung/Einkaufsplanung/Classes/Position.cs
+++ Einkaufsplanung/Einkaufsplanung/Einkaufsplanung/Classes/Position.cs
@@ -6,5 +6,26 @@ namespace Einkaufsplanung.Classes
 {
     class Position
     {
+        /// <summary>
+        /// Int that contains a number assigned to a shelf
+        /// </summary>
+        int shelf;
+
+        /// <summary>
+        /// Int that contains a number assigned to a column in a shelf 
+        /// </summary>
+        int column;
+
+        /// <summary>
+        /// Array that contains all Products in a column of a shelf
+        /// </summary>
+        Product[] listofProducts;
+
+        public Position(int shelf, int column, Product[] listofProducts)
+        {
+            this.shelf = shelf;
+            this.column = column;
+
+        }
     }
 }
--- Einkaufsplanung/Einkaufsplanung/Einkaufsplanung/Classes/Shoppinglist.cs
+++ Einkaufsplanung/Einkaufsplanung/Einkaufsplanung/Classes/Shoppinglist.cs
@@ -5,6 +5,6 @@ using System.Text;
 namespace Einkaufsplanung.Classes
 { 
     class Shoppinglist { 
-    
+        
     }
 }
