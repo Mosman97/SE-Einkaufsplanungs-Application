13311ef50a637a31739b22530a43ada3665d4d6b
Mosman97 <Mosman97@users.noreply.github.com>
2019-05-27 14:52:29 +0200

    Minor changes

--- Einkaufsplanung/Einkaufsplanung/Einkaufsplanung/Classes/Shoppinglist.cs
+++ Einkaufsplanung/Einkaufsplanung/Einkaufsplanung/Classes/Shoppinglist.cs
@@ -1,10 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace Einkaufsplanung.Classes
 { 
-    class Shoppinglist { 
-        
+    class Shoppinglist
+    {
+
+        private string listname_;
+
+        public List<Product> produkte_ = new List<Product>();
+        public static void SaveList(string name)
+        {
+            var backingFile = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "count.txt");
+        }
+
+        public static void LoadList(string name)
+        {
+            
+        }
+
+        public static void DeleteList(string name)
+        {
+            
+        }
+
+        public void AddProduct(Product product)
+        {
+            produkte_.Add(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            produkte_.Remove(product);
+        }
     }
 }
