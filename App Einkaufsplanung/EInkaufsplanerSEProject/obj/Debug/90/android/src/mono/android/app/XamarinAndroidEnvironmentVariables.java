27b682eaffeff0d3f8ce45f35efce3f49bd30c1d
Lat3magnet1c <49225016+Lat3magnet1c@users.noreply.github.com>
2019-06-18 16:08:51 +0200

    Fixes

---
+++ App Einkaufsplanung/EInkaufsplanerSEProject/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "9f5f8ccb-cb17-47b7-8413-ff160bb9caab",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep-conc",
+
+	};
+}
