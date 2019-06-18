7ec439c566072648993b1582c29b9a623240d472
Lat3magnet1c <49225016+Lat3magnet1c@users.noreply.github.com>
2019-06-18 15:39:11 +0200

    Fixes

---
+++ App Einkaufsplanung/EInkaufsplanerSEProject/Classes/LoadListActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace EInkaufsplanerSEProject
+{
+    [Activity(Label = "LoadListActivity")]
+    public class LoadListActivity : Activity
+    {
+        private List<string> mItems;
+        private ListView mListView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.loadlistview);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.main_toolbar);
+            SetActionBar(toolbar);
+            ActionBar.Title = "Load list";
+
+            //Load products from File/s here
+            mItems = Classes.ListStorer.loadListNames();
+            
+
+            mListView = FindViewById<ListView>(Resource.Id.loadListListView);
+
+            LoadListAdapter adapter = new LoadListAdapter(this, mItems);
+
+            mListView.Adapter = adapter;
+
+            //mListView.ItemClick += MListView_ItemClick;
+
+        }
+
+        private void MListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            //Needs implementation to write item to list and return to Shoppinglist listview
+            string auswahl = mItems[e.Position];
+            Console.WriteLine(mItems[e.Position]);
+        }
+    }
+}
---
+++ App Einkaufsplanung/EInkaufsplanerSEProject/Classes/LoadListAdapter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace EInkaufsplanerSEProject
+{
+    class LoadListAdapter : BaseAdapter<string>
+    {
+
+        private List<string> mItems;
+        private Context mContext;
+        protected Activity Context = null;
+
+        public LoadListAdapter(Activity context, List<string> items)
+        {
+            mItems = items;
+            mContext = context;
+            this.Context = context;
+        }
+
+        public override int Count { get { return mItems.Count; } }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override string this[int position] //muss angepasst werden an das Objekt (Später Product statt string)
+        {
+            get { return mItems[position]; }
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent) //https://youtu.be/OHvY1DUxzfo?t=475
+        {
+            View row = convertView;
+
+            if (row == null)
+            {
+                row = Context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem1, null);
+
+                row = LayoutInflater.From(mContext).Inflate(Resource.Layout.loadlistrow, null, false);
+            }
+
+
+
+            TextView Name = row.FindViewById<TextView>(Resource.Id.listName);
+            Name.Text = mItems[position];
+
+            return row;
+
+        }
+    }
+}
