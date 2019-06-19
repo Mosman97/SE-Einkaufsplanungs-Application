fe7bbb3a24802c9bab6273ed515a3d87a2809b6b
Mosman97 <Mosman97@users.noreply.github.com>
2019-06-19 10:31:00 +0200

    add database function

--- App Einkaufsplanung/EInkaufsplanerSEProject/Classes/database.cs
+++ App Einkaufsplanung/EInkaufsplanerSEProject/Classes/database.cs
@@ -60,5 +60,44 @@ namespace EInkaufsplanerSEProject.Classes
 
             return rProducts;
         }
+
+        public static Product returnProduct(string name)
+        {
+            Product produkt = new Product();
+
+            AssetManager assets = Android.App.Application.Context.Assets;
+
+            using (var reader = new StreamReader(assets.Open("database.csv")))
+            using (var csv = new CsvReader(reader))
+            {
+                //rProducts = csv.GetRecords<Product>();
+                csv.Configuration.Delimiter = ";";
+
+
+                csv.Read();
+                csv.ReadHeader();
+
+                while (csv.Read())
+                {
+                    Product record = new Product
+                    {
+                        Category = csv.GetField(0),
+                        Name = csv.GetField(1),
+                        Pos_x = csv.GetField<int>(2),
+                        Pos_y = csv.GetField<int>(3)
+                    };
+
+                    if (name == record.Category)
+                    {
+                        produkt = record;
+                        break;
+                    }
+
+                }
+
+            }
+
+            return produkt;
+        }
     }
 }
