90237ffc7e217e6892a916aa8d8c2f582ae230f5
Annika <fanta99a@yahoo.de>
2019-06-20 23:02:31 +0200

    Store List angepasst.

--- App Einkaufsplanung/EInkaufsplanerSEProject/Classes/ListStorer.cs
+++ App Einkaufsplanung/EInkaufsplanerSEProject/Classes/ListStorer.cs
@@ -24,24 +24,40 @@ namespace EInkaufsplanerSEProject.Classes
 
     }
 
-    public static void StoreList(Shoppinglist shoppinglist)
+    public static void StoreList(Shoppinglist shoplist)
     {
-        AssetManager assets = Android.App.Application.Context.Assets;
-            
-      using (StreamWriter sw = new StreamWriter(assets.Open("lists.csv")))
+      AssetManager assets = Android.App.Application.Context.Assets;
+
+      // Erst alle Shoppinglisten einlesen
+      List<Shoppinglist> Listen = new List<Shoppinglist>();
+      List<string> Names = loadListNames();
+      foreach (string nam in Names)
       {
-            if (LoadList(shoppinglist.Name) == null)
-            {
-                sw.WriteLine(shoppinglist.Name);
-                foreach (Product prod in shoppinglist.Products)
-                {
-                    sw.WriteLine(prod.Name + ";" + prod.Category + ";" + prod.Pos_x + ";" + prod.Pos_y);
-                }
-            }
-            
-        
+        Listen.Add(LoadList(nam));
       }
 
+      //Alle Listen bis auf die geänderte in die Datei schreiben
+      using (StreamWriter sw = new StreamWriter(assets.Open("lists.csv")))
+      {
+        foreach (Shoppinglist shp in Listen)
+        {
+          if (shp.Name != shoplist.Name) // geänderte Datei wird nicht geschrieben
+          {
+            sw.WriteLine(shp.Name);
+            foreach (Product prod in shp.Products)
+            {
+              sw.WriteLine(prod.Name + ";" + prod.Category + ";" + prod.Pos_x + ";" + prod.Pos_y);
+            }
+          }
+        }
+
+        // Als letztes wird die geänderte Liste wieder in die Datei schreiben
+        sw.WriteLine(shoplist.Name);
+        foreach (Product prod in shoplist.Products)
+        {
+          sw.WriteLine(prod.Name + ";" + prod.Category + ";" + prod.Pos_x + ";" + prod.Pos_y);
+        }
+      }
     }
 
     public static List<string> loadListNames()
